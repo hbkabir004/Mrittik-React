6ebfa90f3739b782a07b2856b1077b4708a54030
ABIR <abircse004@gmail.com>
2023-02-19 17:15:35 +0600

    Home-04 pages added

--- src/Pages/Home04.jsx
+++ src/Pages/Home04.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Blogs from '../containers/Blogs';
 import FormContainer from '../containers/FormContainer';
+import Projects04 from '../containers/Projects04';
 import Sliders04 from '../containers/Sliders04';
 import Testimonials from '../containers/Testimonials';
 
@@ -8,6 +9,7 @@ const Home04 = () => {
     return (
         <div className="wrapper">
             <Sliders04 />
+            <Projects04 />
             <Testimonials />
             <Blogs />
             <FormContainer />
