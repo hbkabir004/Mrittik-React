051bdfeadf40f2873b544cd66eaf48928c299db1
ABIR <abircse004@gmail.com>
2023-02-21 01:11:36 +0600

    Team Details added

--- src/components/PointOrder02.jsx
+++ src/components/PointOrder02.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const PointOrder02 = (props) => {
     return (
         <ul className="point_order">
-            <li>{props.li1} <span>{props.span1}</span></li>
+            {/* <li>{props.li1} <span>{props.span1}</span></li>
             <li>{props.li2} <span>{props.span1}</span></li>
-            <li>{props.li3} <span>{props.span2}</span></li>
+            <li>{props.li3} <span>{props.span2}</span></li> */}
         </ul>
     );
 };
