72c9f0d7c606601359b997fe9376f2bb35d1d03c
ABIR <abircse004@gmail.com>
2023-02-26 16:33:04 +0600

    forJSON Shops Pages Added

--- src/components/forJSON/ProductItem.js
+++ src/components/forJSON/ProductItem.js
@@ -11,7 +11,7 @@ const ProductItem = ({productItem}) => {
                     <img className="primary_img" src={img} alt="img" />
                     <div className="cart_button">
                         <a href="#" className="button">
-                            Add to Cart
+                            Add to Car
                         </a>
                     </div>
                 </div>
