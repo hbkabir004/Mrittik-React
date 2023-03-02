04220e6c66e1956e1db76210d11a0b2faf20293b
ABIR <abircse004@gmail.com>
2023-03-02 18:50:36 +0600

    Issuses Resolved

--- src/containers/MiniCart.jsx
+++ src/containers/MiniCart.jsx
@@ -24,44 +24,33 @@ const MiniCart = (props) => {
 
 
     return (
-        <>
-            {/* <button className="navbar-toggler me-3 position-relative" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
-            >
-                <span className="bi bi-cart-dash-fill" />
-                <p className='position-absolute top-0 start-100 translate-middle rounded-circle cart-length-bg'>{cart.length}</p>
+        <div className="offcanvas offcanvas-end" tabindex="-1" id={props.id} aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Cart</h5>
+                <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+                <div className="row gutter-3">
+                    {cart.map(product => (
+                        <MiniCartContent
+                            key={product.id}
+                            product={product}
+                            handleRemoveItem={handleRemoveItem}
+                        />
+                    ))}
 
-
-            </button> */}
-            <div className="offcanvas offcanvas-end" tabindex="-1" id={props.id} aria-labelledby="offcanvasNavbarLabel">
-                <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Cart</h5>
-                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-                <div className="offcanvas-body">
-                    <div className="row gutter-3">
-                        {cart.map(product => (
-                            <MiniCartContent
-                                key={product.id}
-                                product={product}
-                                handleRemoveItem={handleRemoveItem}
-                            />
-                        ))}
-
-                    </div>
-                    <div className="container mt-5">
-                        {/* <hr /> */}
-                        <div className="row gutter-0">
-                            <div className="cart_button d-flex justify-content-center">
-                                <Link to="/shop-cart"><button className="button me-3">View Cart</button></Link>
-
-                                <Link to="/shop-checkout" ><button className="button">Check Out</button></Link>
-                            </div>
+                <div className="container mt-5">
+                    <div className="row gutter-0">
+                        <div className="cart_button d-flex justify-content-center">
+                            <Link to="/shop-cart"><button className="button me-3">View Cart</button></Link>
+                            <Link to="/shop-checkout" ><button className="button">Check Out</button></Link>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-        </>
     );
 };
 
