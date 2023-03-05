d2d33cb12da3eb4a4c6b16a29f72eebc22a09e1b
ABIR <abircse004@gmail.com>
2023-03-05 20:32:44 +0600

    Swiper Resolved

--- src/components/SliderCard.jsx
+++ src/components/SliderCard.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 
 const SliderCard = (props) => {
     return (
-        <div className="swiper-slide">
-            <div className="slider" style={{ backgroundImage: `url(${props.bgImg})` }}>
-                <div className="slide_content">
-                    <div className="slide_content_wrapper mb-0 h-auto bg-dark-100">
-                        <div className="slide_content_inner">
-                            <div className="meta m-0">
-                                <div className="category text-olive text-uppercase">Condominium</div>
-                            </div>
-                            <h4><a href="/project-details" className="text-white">California young menz club in the city center</a></h4>
-                            <div className="details_link">
-                                <a href="/project-details">
-                                    <span className="link_text">View Details</span>
-                                    <span className="link_icon">
-                                        <span className="line"></span>
-                                        <span className="circle"></span>
-                                        <span className="dot"></span>
-                                    </span>
-                                </a>
-                            </div>
+        <div className="slider" style={{ backgroundImage: `url(${props.bgImg})` }}>
+            <div className="slide_content">
+                <div className="slide_content_wrapper mb-0 h-auto bg-dark-100">
+                    <div className="slide_content_inner">
+                        <div className="meta m-0">
+                            <div className="category text-olive text-uppercase">Condominium</div>
+                        </div>
+                        <h4><a href="/project-details" className="text-white">California young menz club in the city center</a></h4>
+                        <div className="details_link">
+                            <a href="/project-details">
+                                <span className="link_text">View Details</span>
+                                <span className="link_icon">
+                                    <span className="line"></span>
+                                    <span className="circle"></span>
+                                    <span className="dot"></span>
+                                </span>
+                            </a>
                         </div>
                     </div>
                 </div>
--- src/containers/Sliders.jsx
+++ src/containers/Sliders.jsx
@@ -1,80 +1,50 @@
-import React, { useEffect } from 'react';
-import Swiper from 'swiper';
+import React from 'react';
+import SwiperCore, { Navigation, Pagination } from "swiper/core";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import SliderCard from '../components/SliderCard';
 
+
+SwiperCore.use([Navigation, Pagination]);
+
 const Sliders = () => {
-    useEffect(() => {
-        // Slider 1
-        var swiper1 = new Swiper('.swiper_theme_slider', {
-            slidesPerView: '1',
-            // centeredSlides: true,
-            autoplay: {
-                delay: 200,
-            },
-            speed: 1600,
-            spaceBetween: 50,
-            parallax: true,
-            autHeight: true,
-            mousewheel: false,
-            effect: 'slide',
-            controller: {
-                inverse: true,
-            },
-            slideToClickedSlide: true,
-            lazyLoading: true,
-            loop: true,
-            keyboard: {
-                enabled: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                dynamicBullets: true,
-            },
-
-            breakpoints: {
-                767: {
-                    // slidesPerView: 1.5,
-                    spaceBetween: 70,
-                },
-                1200: {
-                    // slidesPerView: 1.5,
-                    spaceBetween: 120,
-                },
-                1400: {
-                    // slidesPerView: 1.5,
-                    spaceBetween: 170,
-                },
-            }
-        });
-
-        // $(window).resize(function() {
-        //     var bodyheight = $(this).height();
-        //     $(".swiper_theme_slider").height(bodyheight);
-        // }).resize();
-
-    }, []);
+    const swiper = useSwiper();
     return (
         <div className="theme_slider bg-black">
             <div className="container">
                 <div className="swiper swiper_theme_slider" data-swiper-autoplay="2000">
-                    <div className="swiper-wrapper">
-                        <SliderCard bgImg="images/slider/14.jpg" />
-                        <SliderCard bgImg="images/slider/5.jpg" />
-                        <SliderCard bgImg="images/slider/8.jpg" />
-                    </div>
+                    {/* <div className="swiper-wrapper"> */}
+                    <Swiper
+                        // modules={[Navigation, Pagination]}
+                        slidesPerView={1}
+                        loop={true}
+                        initialSlide={1}
+                        centeredSlides={true}
+                        navigation
+                        pagination={{
+                            clickable: true,
+                            dynamicBullets: true,
+                        }}
+                    >
+                        <SwiperSlide>
+                            <SliderCard bgImg="images/slider/14.jpg" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <SliderCard bgImg="images/slider/5.jpg" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <SliderCard bgImg="images/slider/8.jpg" />
+                        </SwiperSlide>
+                    </Swiper>
+
+                    {/* </div> */}
                     {/* <!-- Add Pagination --> */}
-                    <div className="swiper-pagination"></div>
+                    {/* <div className="swiper-pagination"></div> */}
 
                     {/* <!-- Add Buttons --> */}
-                    <div className="swiper-navigation">
+                    {/* <div className="swiper-navigation">
                         <div className="swiper-button-prev"><i className="bi bi-arrow-left"></i></div>
                         <div className="swiper-button-next"><i className="bi bi-arrow-right"></i></div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             <div className="divider_bg bg-dark-100"></div>
