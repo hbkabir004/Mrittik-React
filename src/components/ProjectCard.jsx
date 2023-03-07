e6df08abbf91aa9e5d9042ac60c48a49f0799779
ABIR <abircse004@gmail.com>
2023-03-07 14:43:31 +0600

    _redirects Added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import LazyLoad from 'react-lazyload';
 const ProjectCard = (props) => {
     return (
         <div className="thumb">
-            <LazyLoad height={600} offset={100} once>
+            <LazyLoad height={700} once>
                 <img className="item_image" src={props.img} alt="" />
             </LazyLoad>
             <div className="works-info">
