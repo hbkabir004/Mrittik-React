48a12ceb3d28272fe06f6d473889a419fe849825
ABIR <abircse004@gmail.com>
2023-03-14 18:57:54 +0600

    Issues Resolved

--- src/containers/Projects03.jsx
+++ src/containers/Projects03.jsx
@@ -1,4 +1,3 @@
-import Aos from 'aos';
 import Isotope from 'isotope-layout';
 import React, { useEffect } from 'react';
 import ProjectCard03 from '../components/ProjectCard03';
@@ -22,46 +21,47 @@ const Projects03 = () => {
         // cleanup
         return () => Isotope.current.destroy()
     }, []);
-    Aos.init();
+
+
     return (
         <section className="projects masonry bg-transparent effect-tilt p-0">
             <div className="container">
                 <div className="grid grid-2 gutter-90 clearfix">
                     <div className="grid-sizer"></div>
-                    <div className="grid-item residences" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item residences">
                         <ProjectCard03 img="images/portfolio/1.jpg" details="California young menz club" />
                     </div>
-                    <div className="grid-item interiors" data-aos="fade-up" data-aos-duration="900">
+                    <div className="grid-item interiors">
                         <ProjectCard03 img="images/portfolio/2.jpg" details="Sunlight in the Room" />
                     </div>
-                    <div className="grid-item residences" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item residences">
                         <ProjectCard03 img="images/portfolio/3.jpg" details="Find your own self invintage lake house" />
                     </div>
-                    <div className="grid-item landscape exterior" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item landscape exterior">
                         <ProjectCard03 img="images/portfolio/4.jpg" details="Well decor house in Sydney" />
                     </div>
-                    <div className="grid-item landscape" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item landscape">
                         <ProjectCard03 img="images/portfolio/5.jpg" details="Huge large area Bedroom" />
                     </div>
-                    <div className="grid-item interiors" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item interiors">
                         <ProjectCard03 img="images/portfolio/6.jpg" details="Clean water in the swiming pool" />
                     </div>
-                    <div className="grid-item interiors" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item interiors">
                         <ProjectCard3_2 img="images/portfolio/7.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item residences" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item residences">
                         <ProjectCard3_2 img="images/portfolio/7.jpg" details="Newyork golf club house"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item exterior" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item exterior">
                         <ProjectCard3_2 img="images/portfolio/1.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item interiors" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item interiors">
                         <ProjectCard3_2 img="images/portfolio/2.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item residences" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item residences">
                         <ProjectCard3_2 img="images/portfolio/3.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item landscape exterior" data-aos="fade-up" data-aos-duration="700">
+                    <div className="grid-item landscape exterior">
                         <ProjectCard3_2 img="images/portfolio/4.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
                 </div>
