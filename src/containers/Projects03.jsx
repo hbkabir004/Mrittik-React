e45342c0ce4f2811547a0cf5a735e03f560d5145
ABIR <abircse004@gmail.com>
2023-03-16 17:48:07 +0600

    Home-04 removed

--- src/containers/Projects03.jsx
+++ src/containers/Projects03.jsx
@@ -1,73 +1,52 @@
-import Isotope from 'isotope-layout';
-import React, { useEffect } from 'react';
+import React from 'react';
 import ProjectCard03 from '../components/ProjectCard03';
 import ProjectCard3_2 from '../components/ProjectCard03_2';
 
 const Projects03 = () => {
-    useEffect(() => {
-        Isotope.current = new Isotope('.grid', {
-            itemSelector: '.grid-item',
-            percentPosition: true,
-            masonry: {
-                columnWidth: '.grid-sizer'
-            },
-            transformsEnabled: true,
-            transitionDuration: "700ms",
-            resize: true,
-            fitWidth: true,
-            // columnWidth: '.grid-sizer',
-        })
-
-        // cleanup
-        return () => Isotope.current.destroy()
-    }, []);
-
-
     return (
-        <section className="projects masonry bg-transparent effect-tilt p-0">
+        <section className="projects bg-transparent effect-tilt p-0">
             <div className="container">
-                <div className="grid grid-2 gutter-90 clearfix">
-                    <div className="grid-sizer"></div>
-                    <div className="grid-item residences">
+                <div className="row row-cols-2 gutter-90 clearfix">
+                    <div className="grid-item col residences">
                         <ProjectCard03 img="images/portfolio/1.jpg" details="California young menz club" />
                     </div>
-                    <div className="grid-item interiors">
+                    <div className="grid-item col interiors">
                         <ProjectCard03 img="images/portfolio/2.jpg" details="Sunlight in the Room" />
                     </div>
-                    <div className="grid-item residences">
+                    <div className="grid-item col residences">
                         <ProjectCard03 img="images/portfolio/3.jpg" details="Find your own self invintage lake house" />
                     </div>
-                    <div className="grid-item landscape exterior">
+                    <div className="grid-item col landscape exterior">
                         <ProjectCard03 img="images/portfolio/4.jpg" details="Well decor house in Sydney" />
                     </div>
-                    <div className="grid-item landscape">
+                    <div className="grid-item col landscape">
                         <ProjectCard03 img="images/portfolio/5.jpg" details="Huge large area Bedroom" />
                     </div>
-                    <div className="grid-item interiors">
+                    <div className="grid-item col interiors">
                         <ProjectCard03 img="images/portfolio/6.jpg" details="Clean water in the swiming pool" />
                     </div>
-                    <div className="grid-item interiors">
+                    <div className="grid-item col interiors">
                         <ProjectCard3_2 img="images/portfolio/7.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item residences">
+                    <div className="grid-item col residences">
                         <ProjectCard3_2 img="images/portfolio/7.jpg" details="Newyork golf club house"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item exterior">
+                    <div className="grid-item col exterior">
                         <ProjectCard3_2 img="images/portfolio/1.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item interiors">
+                    <div className="grid-item col interiors">
                         <ProjectCard3_2 img="images/portfolio/2.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item residences">
+                    <div className="grid-item col residences">
                         <ProjectCard3_2 img="images/portfolio/3.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item landscape exterior">
+                    <div className="grid-item col landscape exterior">
                         <ProjectCard3_2 img="images/portfolio/4.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
                 </div>
-                <div className="btn_group w-100 text-center">
+                {/* <div className="btn_group w-100 text-center">
                     <button id="load-more" className="btn black w-100">Discover All Projects</button>
-                </div>
+                </div> */}
             </div>
         </section>
     );
--- src/containers/Sliders04.jsx
+++ src/containers/Sliders04.jsx
@@ -6,43 +6,6 @@ import SliderCard04 from '../components/SliderCard04';
 SwiperCore.use([Pagination, Scrollbar, Autoplay]);
 
 const Sliders04 = () => {
-    // useEffect(() => {
-    //     // Slider 4
-    //     var swiper4 = new Swiper('.swiper_theme_slider_4', {
-    //         slidesPerView: '1',
-    //         centeredSlides: true,
-    //         speed: 1600,
-    //         autoplay: true,
-    //         spaceBetween: 0,
-    //         parallax: true,
-    //         autHeight: true,
-    //         mousewheel: false,
-    //         effect: 'slide',
-    //         controller: {
-    //             inverse: true,
-    //         },
-    //         slideToClickedSlide: true,
-    //         lazyLoading: true,
-    //         loop: false,
-    //         keyboard: {
-    //             enabled: true,
-    //         },
-    //         navigation: {
-    //             nextEl: '.swiper-button-next',
-    //             prevEl: '.swiper-button-prev',
-    //         },
-    //         pagination: {
-    //             el: '.swiper-pagination',
-    //             clickable: true,
-    //             dynamicBullets: true,
-    //         },
-    //     });
-
-    //     $(window).resize(function () {
-    //         var bodyheight = $(this).height();
-    //         $(".swiper_theme_slider_4").height(bodyheight);
-    //     }).resize();
-    // }, []);
     return (
         <div className="theme_slider_4 p-0">
             <div className="swiper swiper_theme_slider_4">
@@ -61,23 +24,15 @@ const Sliders04 = () => {
                     }}
                 >
                     <SwiperSlide>
-                        <SliderCard04 bgImg="images/slider/6.jpg" title="The Future Architecture Is Here" slide="A" />
+                        <SliderCard04 bgImg02="images/slider/6.jpg" title="The Future Architecture Is Here" slide="A" />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <SliderCard04 bgImg="images/slider/2.jpg" title="We value your Dream" slide="D" />
+                        <SliderCard04 bgImg02="images/slider/2.jpg" title="We value your Dream" slide="D" />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <SliderCard04 bgImg="images/slider/4.jpg" title="Make true your Dream with Mrittik" slide="T" />
+                        <SliderCard04 bgImg02="images/slider/4.jpg" title="Make true your Dream with Mrittik" slide="T" />
                     </SwiperSlide>
                 </Swiper>
-
-
-                {/* <!-- Add Pagination --> */}
-                {/* <div className="swiper-pagination"></div> */}
-
-                {/* <!-- Add Buttons -->
-            <!-- <div className="swiper-button-prev details_link l-dir pagination-previous"><a href="#"><span className="link_icon"><span className="line"></span> <span className="circle"></span><span className="dot"></span></span></a></div>
-            <div className="swiper-button-next details_link r-dir pagination-next"><a href="#"><span className="link_icon"><span className="line"></span> <span className="circle"></span><span className="dot"></span></span></a></div> --> */}
 
             </div>
         </div>
