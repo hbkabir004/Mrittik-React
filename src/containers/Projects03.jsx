2f8b61ee5c3cb445605008b08d113d7a04d6169c
ABIR <hbkabir004@gmail.com>
2023-03-18 01:07:40 +0600

    Home-07 issuses solved

--- src/containers/Projects03.jsx
+++ src/containers/Projects03.jsx
@@ -6,41 +6,41 @@ const Projects03 = () => {
     return (
         <section className="projects top-margin bg-transparent effect-tilt p-0">
             <div className="container">
-                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-2 gutter-90 clearfix">
-                    <div className="grid-item col residences">
+                <div className="row gutter-90 clearfix">
+                    <div className="grid-item col-6 residences">
                         <ProjectCard03 img="images/portfolio/1.jpg" details="California young menz club" />
                     </div>
-                    <div className="grid-item col interiors">
+                    <div className="grid-item col-6 interiors">
                         <ProjectCard03 img="images/portfolio/2.jpg" details="Sunlight in the Room" />
                     </div>
-                    <div className="grid-item col residences">
+                    <div className="grid-item col-6 residences">
                         <ProjectCard03 img="images/portfolio/3.jpg" details="Find your own self invintage lake house" />
                     </div>
-                    <div className="grid-item col landscape exterior">
+                    <div className="grid-item col-6 landscape exterior">
                         <ProjectCard03 img="images/portfolio/4.jpg" details="Well decor house in Sydney" />
                     </div>
-                    <div className="grid-item col landscape">
+                    <div className="grid-item col-6 landscape">
                         <ProjectCard03 img="images/portfolio/5.jpg" details="Huge large area Bedroom" />
                     </div>
-                    <div className="grid-item col interiors">
+                    <div className="grid-item col-6 interiors">
                         <ProjectCard03 img="images/portfolio/6.jpg" details="Clean water in the swiming pool" />
                     </div>
-                    <div className="grid-item col interiors">
+                    <div className="grid-item col-6 interiors">
                         <ProjectCard3_2 img="images/portfolio/7.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item col residences">
+                    <div className="grid-item col-6 residences">
                         <ProjectCard3_2 img="images/portfolio/8.jpg" details="Newyork golf club house"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item col exterior">
+                    <div className="grid-item col-6 exterior">
                         <ProjectCard3_2 img="images/portfolio/1.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item col interiors">
+                    <div className="grid-item col-6 interiors">
                         <ProjectCard3_2 img="images/portfolio/2.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item col residences">
+                    <div className="grid-item col-6 residences">
                         <ProjectCard3_2 img="images/portfolio/3.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
-                    <div className="grid-item col landscape exterior">
+                    <div className="grid-item col-6 landscape exterior">
                         <ProjectCard3_2 img="images/portfolio/4.jpg" details="California young menz club"></ProjectCard3_2>
                     </div>
                 </div>
